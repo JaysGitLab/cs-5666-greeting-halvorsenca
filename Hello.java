8604b7da3641aca5d9beb2daf9a9b50fd10ed4eb
Chad Halvorsen <halvorsenca@appstate.edu>
2017-01-31 21:48:21 -0500

    Say hello

---
+++ Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+     public Hello() {
+         sayHello();
+     }
+
+     public void sayHello() {
+         System.out.println("Hello, World!");
+     }
+     public static void main(String[] args) {
+         Hello hello = new Hello();
+     }
+ }
+
