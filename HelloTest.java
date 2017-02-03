9c4a40d578646d37870093af77aea9f03a1143a6
Chad Halvorsen <halvorsenca@appstate.edu>
2017-02-03 09:06:50 -0500

    Test hello

---
+++ HelloTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class HelloTest {
+    @Test
+    public void evaluatesSayHelloToMe() {
+        Hello hello = new Calculator()
+
