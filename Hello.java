7f63a111abd6278f754876185d55c3c9d30ec283
Chad Halvorsen <halvorsenca@appstate.edu>
2017-02-05 15:36:19 -0500

    say hello to me

--- Hello.java
+++ Hello.java
@@ -1,12 +1,12 @@
 public class Hello {
     public Hello(){
-        sayHelloToMe()
     }
     public void sayHelloToMe() {
-        System.out.println("Hello Chad");
+        System.out.println("Hello, Chad");
     }
     public static void main(String[] args) {
         Hello hello = new Hello();
+        hello.sayHelloToMe();
     }
 }
 
--- HelloTest.java
+++ HelloTest.java
@@ -1,8 +1,19 @@
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+import java.io.*;
 
 public class HelloTest {
     @Test
     public void evaluatesSayHelloToMe() {
-        Hello hello = new Calculator()
+        Hello hello = new Hello();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(baos);
+        PrintStream old = System.out;
+        System.setOut(ps); 
+        hello.sayHelloToMe();
+        System.out.flush();
+        System.setOut(old);
+        assertEquals("Hello, Chad\n", baos.toString());
+    }
+}
 
